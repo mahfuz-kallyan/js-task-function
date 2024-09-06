db3b62899736203f2da7ca23326f286afdbd2a85
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-09-06 18:38:31 +0600

    task 4 done

--- task4.js
+++ task4.js
@@ -1,3 +1,13 @@
-function count_zero('number'){
-      
+function count_zero(bString) {
+    let result = 0;
+    const bArray = bString.split('')
+    console.log(bArray)
+    for (const el of bArray) {
+        if (parseInt(el) === 0) {
+            result++
+        }
+    }
+    return result;
 }
+
+console.log(count_zero('10001000010000'))
