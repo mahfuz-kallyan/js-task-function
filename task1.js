c2e5df81c9c959a8f5d430ff8905f759a4e8ae70
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-09-06 15:54:50 +0600

    added total

--- task1.js
+++ task1.js
@@ -1,5 +1,15 @@
+
+// multiple by function
 function multi(num1, num2, num3, num4){
     const sum = num1 * num2 * num3 * num4;
     console.log(sum);
 }
 multi(3, 5, 2, 4);
+
+// add by function return
+
+function add(num1, num2, num3){
+    const total = num1 + num2 + num3;
+    return total;
+}
+console.log(add(45, 20, 25));
